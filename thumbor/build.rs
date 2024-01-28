6c7e7523dd2ef76a77270b0f3963829fd5b0e275
yjh <860622588@qq.com>
2024-01-28 22:03:15 +0800

    feat: init thumbor

---
+++ thumbor/build.rs
@@ -0,0 +1,14 @@
+use std::process::Command;
+
+fn main() {
+    prost_build::Config::new()
+        .out_dir("src/pb")
+        .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .compile_protos(&["abi/abi.proto"], &["."])
+        .unwrap();
+
+    Command::new("cargo")
+        .args(&["fmt", "--", "src/*.rs"])
+        .status()
+        .expect("cargo fmt failed");
+}
---
+++ thumbor/src/pb/mod.rs
@@ -0,0 +1,112 @@
+mod abi;
+pub use abi::*;
+use anyhow::Error;
+use base64::{engine::general_purpose, Engine};
+use photon_rs::transform::SamplingFilter;
+use std::str::FromStr;
+
+impl ImageSpec {
+    pub fn new(specs: Vec<Spec>) -> Self {
+        Self { specs }
+    }
+}
+
+impl From<&ImageSpec> for String {
+    fn from(image_spec: &ImageSpec) -> Self {
+        let image_spec = serde_json::to_string(image_spec).unwrap();
+
+        let encode = general_purpose::URL_SAFE_NO_PAD.encode(image_spec);
+
+        encode
+    }
+}
+
+impl FromStr for ImageSpec {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let json = String::from_utf8(general_purpose::URL_SAFE_NO_PAD.decode(s)?)?;
+
+        let image_spec: ImageSpec = serde_json::from_str(&json)?;
+
+        Ok(image_spec)
+    }
+}
+
+impl filter::Filter {
+    pub fn to_str(&self) -> Option<&'static str> {
+        match self {
+            filter::Filter::Islands => Some("islands"),
+            filter::Filter::Marine => Some("marine"),
+            filter::Filter::Oceanic => Some("oceanic"),
+            filter::Filter::Unspecified => None,
+        }
+    }
+}
+
+impl From<resize::SampleFilter> for SamplingFilter {
+    fn from(v: resize::SampleFilter) -> Self {
+        match v {
+            resize::SampleFilter::Undefined => SamplingFilter::Nearest,
+            resize::SampleFilter::Nearest => SamplingFilter::Nearest,
+            resize::SampleFilter::Triangle => SamplingFilter::Triangle,
+            resize::SampleFilter::CatmullRom => SamplingFilter::CatmullRom,
+            resize::SampleFilter::Gaussian => SamplingFilter::Gaussian,
+            resize::SampleFilter::Lanczos3 => SamplingFilter::Lanczos3,
+        }
+    }
+}
+
+impl Spec {
+    pub fn new_resize_seam_carve(width: u32, height: u32) -> Self {
+        Self {
+            data: Some(spec::Data::Resize(Resize {
+                width,
+                height,
+                rtype: resize::ResizeType::SeamCarve as i32,
+                filter: resize::SampleFilter::Undefined as i32,
+            })),
+        }
+    }
+
+    pub fn new_resize(width: u32, height: u32, filter: resize::SampleFilter) -> Self {
+        Self {
+            data: Some(spec::Data::Resize(Resize {
+                width,
+                height,
+                rtype: resize::ResizeType::Normal as i32,
+                filter: filter as i32,
+            })),
+        }
+    }
+
+    pub fn new_filter(filter: filter::Filter) -> Self {
+        Self {
+            data: Some(spec::Data::Filter(Filter {
+                filter: filter as i32,
+            })),
+        }
+    }
+
+    pub fn new_watermark(x: u32, y: u32) -> Self {
+        Self {
+            data: Some(spec::Data::Watermark(Watermark { x, y })),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use std::borrow::Borrow;
+
+    use super::*;
+
+    #[test]
+    fn test_image_spec() {
+        let image_spec = super::ImageSpec::new(vec![Spec::new_watermark(10, 20)]);
+        let s: String = image_spec.borrow().into();
+
+        assert_eq!(image_spec, s.as_str().parse::<ImageSpec>().unwrap());
+    }
+}
