7f28320ca488a624aa8e8857be4da9653e53b105
yjh <860622588@qq.com>
2024-02-15 14:15:00 +0800

    feat: add hget、hset、hgetall

--- kv/examples/dummy_server.rs
+++ kv/examples/dummy_server.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use async_prost::AsyncProstStream;
 use futures::prelude::*;
-use kv::{CommandRequest, CommandResponse};
+use kv::{CommandRequest, CommandResponse, MemTable, Service};
 use tokio::net::TcpListener;
 use tracing::info;
 
@@ -9,6 +9,7 @@ use tracing::info;
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
+    let service: Service = Service::new(MemTable::new());
     let addr = "127.0.0.1:9527";
     let listener = TcpListener::bind(addr).await?;
 
@@ -19,18 +20,17 @@ async fn main() -> Result<()> {
 
         info!("Client {:?} connected", addr);
 
+        let svc = service.clone();
+
         tokio::spawn(async move {
             let mut stream =
                 AsyncProstStream::<_, CommandRequest, CommandResponse, _>::from(stream).for_async();
 
-            while let Some(Ok(msg)) = stream.next().await {
-                info!("Got a new command: {:?}", msg);
-                // 创建一个 404 response 返回给客户端
-                let mut resp = CommandResponse::default();
-                resp.status = 404;
-                resp.message = "Not found".to_string();
-                stream.send(resp).await.unwrap();
+            while let Some(Ok(cmd)) = stream.next().await {
+                let res = svc.execute(cmd);
+                stream.send(res).await.unwrap();
             }
+
             info!("Client {:?} disconnected", addr);
         });
     }
