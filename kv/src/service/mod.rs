41f18788f6c090e837719e42c57ace1f6f289cae
yjh <860622588@qq.com>
2024-02-19 10:27:46 +0800

    feat: Service support event subscribe

--- kv/src/service/mod.rs
+++ kv/src/service/mod.rs
@@ -15,12 +15,20 @@ pub trait CommandService {
 /// Service 数据结构
 pub struct Service {
     store: Arc<dyn Storage>,
+    on_received: Vec<fn(&CommandRequest)>,
+    on_executed: Vec<fn(&CommandResponse)>,
+    on_before_send: Vec<fn(&mut CommandResponse)>,
+    on_after_send: Vec<fn()>,
 }
 
 impl Clone for Service {
     fn clone(&self) -> Self {
         Self {
             store: Arc::clone(&self.store),
+            on_received: self.on_received.clone(),
+            on_executed: self.on_executed.clone(),
+            on_before_send: self.on_before_send.clone(),
+            on_after_send: self.on_after_send.clone(),
         }
     }
 }
@@ -29,17 +37,71 @@ impl Service {
     pub fn new<S: Storage>(store: S) -> Self {
         Self {
             store: Arc::new(store),
+            on_received: vec![],
+            on_executed: vec![],
+            on_before_send: vec![],
+            on_after_send: vec![],
         }
+    }
+
+    pub fn fn_received(mut self, f: fn(&CommandRequest)) -> Self {
+        self.on_received.push(f);
+        self
+    }
+
+    pub fn fn_executed(mut self, f: fn(&CommandResponse)) -> Self {
+        self.on_executed.push(f);
+        self
+    }
+
+    pub fn fn_before_send(mut self, f: fn(&mut CommandResponse)) -> Self {
+        self.on_before_send.push(f);
+        self
+    }
+
+    pub fn fn_after_send(mut self, f: fn()) -> Self {
+        self.on_after_send.push(f);
+        self
     }
 
     pub fn execute(&self, cmd: CommandRequest) -> CommandResponse {
         debug!("Got request: {:?}", cmd);
-        // TODO: 发送 on_received 事件
-        let res = dispatch(cmd, &self.store);
+        self.on_received.notify(&cmd);
+        let mut res = dispatch(cmd, &self.store);
         debug!("Executed response: {:?}", res);
-        // TODO: 发送 on_executed 事件
+
+        self.on_executed.notify(&res);
+        self.on_before_send.notify(&mut res);
+
+        if !self.on_before_send.is_empty() {
+            debug!("Modified response: {:?}", res);
+        }
 
         res
+    }
+}
+
+pub trait Notify<Arg> {
+    fn notify(&self, arg: &Arg);
+}
+
+pub trait NotifyMut<Arg> {
+    fn notify(&self, arg: &mut Arg);
+}
+
+impl<Arg> Notify<Arg> for Vec<fn(&Arg)> {
+    fn notify(&self, arg: &Arg) {
+        for f in self {
+            f(arg);
+        }
+    }
+}
+
+impl<Arg> NotifyMut<Arg> for Vec<fn(&mut Arg)> {
+    fn notify(&self, arg: &mut Arg) {
+        for f in self {
+            f(arg)
+        }
     }
 }
 
@@ -63,6 +125,9 @@ pub fn dispatch(cmd: CommandRequest, store: &Arc<dyn Storage>) -> CommandRespons
 mod tests {
     use std::thread;
 
+    use http::StatusCode;
+    use tracing::info;
+
     use super::*;
     use crate::{MemTable, Value};
 
@@ -84,6 +149,35 @@ mod tests {
         // 在当前线程下读取 table t1 的 k1，应该返回 v1
         let res = service.execute(CommandRequest::new_hget("t1", "k1"));
         assert_res_ok(res, &["v1".into()], &[]);
+    }
+
+    #[test]
+    fn event_registration_should_work() {
+        fn b(cmd: &CommandRequest) {
+            info!("Got {:?}", cmd);
+        }
+        fn c(res: &CommandResponse) {
+            info!("{:?}", res);
+        }
+        fn d(res: &mut CommandResponse) {
+            res.status = StatusCode::CREATED.as_u16() as _;
+        }
+        fn e() {
+            info!("Data is sent");
+        }
+
+        let service: Service = Service::new(MemTable::default())
+            .fn_received(|_: &CommandRequest| {})
+            .fn_received(b)
+            .fn_executed(c)
+            .fn_before_send(d)
+            .fn_after_send(e);
+
+        let res = service.execute(CommandRequest::new_hset("t1", "k1", "v1".into()));
+
+        assert_eq!(res.status, StatusCode::CREATED.as_u16() as _);
+        assert_eq!(res.message, "");
+        assert_eq!(res.values, vec![Value::default()]);
     }
 }
 
