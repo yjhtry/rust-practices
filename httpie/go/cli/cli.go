0918ec6bcb45f36cbe7b4db1d214313081a7729b
yjhtry <860622588@qq.com>
2024-05-14 15:39:47 +0800

    feat: go httpie get

--- httpie/go/cli/cli.go
+++ httpie/go/cli/cli.go
@@ -1,12 +1,16 @@
 package cli
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
+	"github.com/fatih/color"
 	"github.com/urfave/cli/v2"
 )
 
@@ -26,21 +30,15 @@ func Run() {
 
 					if err != nil {
 						fmt.Println("Fetch Error: ", err)
+
+						return err
 					}
 
 					defer res.Body.Close()
 
-					for key, value := range res.Header {
-						fmt.Printf("%s: %s\n", key, value)
-					}
-
-					body, err := io.ReadAll(res.Body)
-					if err != nil {
-						fmt.Println("读取响应体失败:", err)
-						return nil
-					}
-
-					fmt.Println(string(body))
+					printStatus(res)
+					printHeaders(res)
+					printBody(res)
 
 					return nil
 				},
@@ -60,4 +58,62 @@ func Run() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func PrettyString(str string) (string, error) {
+	var prettyJSON bytes.Buffer
+
+	if err := json.Indent(&prettyJSON, []byte(str), "", "    "); err != nil {
+		return "", err
+	}
+
+	return prettyJSON.String(), nil
+}
+
+func printStatus(res *http.Response) {
+	var statusCodeText string
+
+	switch res.StatusCode {
+	case 200:
+		statusCodeText = color.GreenString(fmt.Sprint(res.StatusCode))
+	case 400:
+		statusCodeText = color.YellowString(fmt.Sprint(res.StatusCode))
+	case 404, 500:
+		statusCodeText = color.RedString(fmt.Sprint(res.StatusCode))
+	}
+
+	fmt.Println(
+		color.GreenString("Response Status:"),
+		statusCodeText,
+		color.BlueString(fmt.Sprint(res.Proto)),
+	)
+}
+
+func printHeaders(res *http.Response) {
+	for key, value := range res.Header {
+		fmt.Printf("%s: %s\n", color.GreenString(key), color.CyanString(strings.Join(value, ",")))
+	}
+
+	fmt.Println()
+}
+func printBody(res *http.Response) {
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println("读取响应体失败:", err)
+		return
+	}
+
+	stringBody := string(body)
+
+	if strings.Contains(res.Header.Get("Content-Type"), "application/json") {
+		stringJson, err := PrettyString(stringBody)
+
+		if err == nil {
+			stringBody = stringJson
+		}
+	}
+
+	fmt.Println(color.CyanString(stringBody))
+
 }
--- httpie/go/go.mod
+++ httpie/go/go.mod
@@ -2,10 +2,14 @@ module go-cli/httpie
 
 go 1.22.3
 
+require github.com/urfave/cli/v2 v2.27.2 // direct
+
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/fatih/color v1.17.0 // direct
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/urfave/cli/v2 v2.27.2 // direct
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
-
+	golang.org/x/sys v0.18.0 // indirect
 )
